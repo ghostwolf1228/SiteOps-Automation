27e9710eb02de15c44e95a159965d4dbf0051d31
Sam Ransford <ghostwolf1228@users.noreply.github.com>
2016-04-16 22:24:43 -0700

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -243,7 +243,7 @@ function runProcess {
 			if [[ $usrChoice == "as" || $usrChoice == "AS" ]]
 			then
 				printf "Specify a rack, leave blank if you want the whole file: "; read usrRack
-				printf "\n\n$usrRack\n\n"
+				printf "\n\n${usrRack^^}\n\n"
 				exit
 				if [[ -z $usrRack ]]
 				then
